ca1d24ffe3fb11acebca2ce15ff448d8fc188bfa
BastienLeGuellec <54061033+BastienLeGuellec@users.noreply.github.com>
2023-09-15 16:03:57 +0200

    Update snowball.R
    change the variables that are displayed in the final df

--- snowball.R
+++ snowball.R
@@ -73,7 +73,7 @@ snowball_bibliography <- function(id_list, ndisp=50, depth=2, api_key='TdUUUOLUW
     slice(1:ndisp) |>
     rename("lens_id" = "corpus", "Freq" = "n")
   
-  includes = c("lens_id","title", "authors", "abstract", "external_ids", "scholarly_citations", "references", "source", "year_published")
+  includes = c("lens_id","title", "authors", "abstract", "external_ids", "scholarly_citations_count", "source", "year_published")
   
   complete_articles = df_freq$lens_id |>
     as.character() |> 
@@ -85,7 +85,7 @@ snowball_bibliography <- function(id_list, ndisp=50, depth=2, api_key='TdUUUOLUW
     left_join(df_freq, by="lens_id") |> 
     arrange(-Freq) |> 
     pubmed_complete() |> 
-    select(authors,year_published, journal, title, abstract, lens_id, pmid, Freq)
+    select(authors,year_published, journal, title, abstract, scholarly_citations_count, Freq)
   
   for (i in 1:ndisp){
     complete_articles$authors[i]<-paste0(complete_articles$authors[[i]][[3]][[1]], ". ",complete_articles$authors[[i]][[2]][[1]])
