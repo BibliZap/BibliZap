fd04d4f87421e2f782b76172546344404c4c1c0a
BastienLeGuellec <54061033+BastienLeGuellec@users.noreply.github.com>
2023-09-15 16:02:13 +0200

    Update server.R
    change columns that are displayed (+ DOI + Citations -lens id - PMID)

--- server.R
+++ server.R
@@ -34,10 +34,12 @@ server <- function(input, output, session) {
   # Make bibliography_table reactive
    output$bibliography_table <- renderDT({
     bibliography_reactive() |> 
-      mutate(Lens = sprintf('<a href="https://www.lens.org/lens/scholar/article/%s/main" target="_blank">%s</a>', lens_id, lens_id)) |> 
-      mutate(PMID = sprintf('<a href="https://pubmed.ncbi.nlm.nih.gov/%s/" target="_blank">%s</a>', pmid, pmid))|>
-      rename(Title = title, Abstract = abstract, Score = Freq, FirstAuthor = authors, Year = year_published, Journal = journal) |> 
-      select(FirstAuthor, Year, Journal, Title, Abstract, Lens, PMID, Score) |> 
+      #mutate(Lens = sprintf('<a href="https://www.lens.org/lens/scholar/article/%s/main" target="_blank">%s</a>', lens_id, lens_id)) |> 
+      #mutate(PMID = sprintf('<a href="https://pubmed.ncbi.nlm.nih.gov/%s/" target="_blank">%s</a>', pmid, pmid))|>
+      mutate(DOI = sprintf('<a href="https://www.doi.org/%s" target="_blank">%s</a>', doi, doi)) |>
+      rename(Title = title, Abstract = abstract, Score = Freq, FirstAuthor = authors, Year = year_published, Journal = journal, Citations = scholarly_citations_count) |> 
+      select(FirstAuthor, Year, Journal, Title, Abstract, DOI, Score, Citations) |> 
+
       DT::datatable(
         escape = FALSE,
         filter = 'top',
