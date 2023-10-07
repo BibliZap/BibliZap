95405fe6e002f3d2b7fe847ac7eda7db61a2a41a
BastienLeGuellec <54061033+BastienLeGuellec@users.noreply.github.com>
2023-10-07 18:02:51 +0200

    Update server.R
    Add unwanted and tickboxes

--- server.R
+++ server.R
@@ -12,9 +12,10 @@ server <- function(input, output, session) {
     
     depth <- input$depth_slider
     ndisp <- input$ndisp_slider
+    unwanted_id<-input$unwanted_id_list
 
     
-    bibliography <- snowball_bibliography(id_list, ndisp=ndisp, depth=depth)
+    bibliography <- snowball_bibliography(id_list, ndisp=ndisp, depth=depth, unwanted_list=unwanted_id)
     
     # Store bibliography in global environment
     assign("bibliography", bibliography, envir = .GlobalEnv)
@@ -33,21 +34,28 @@ server <- function(input, output, session) {
   })
   
   # Make bibliography_table reactive
-   output$bibliography_table <- renderDT({
-    bibliography_reactive() |> 
+  output$bibliography_table <- renderDT({
+    dt <- bibliography_reactive() |> 
       #mutate(Lens = sprintf('<a href="https://www.lens.org/lens/scholar/article/%s/main" target="_blank">%s</a>', lens_id, lens_id)) |> 
       #mutate(PMID = sprintf('<a href="https://pubmed.ncbi.nlm.nih.gov/%s/" target="_blank">%s</a>', pmid, pmid))|>
       mutate(DOI = sprintf('<a href="https://www.doi.org/%s" target="_blank">%s</a>', doi, doi)) |>
       rename(Title = title, Abstract = abstract, Score = Freq, FirstAuthor = authors, Year = year_published, Journal = journal, Citations = scholarly_citations_count) |> 
-      select(FirstAuthor, Year, Journal, Title, Abstract, DOI, Score, Citations) |> 
-
-      DT::datatable(
-        escape = FALSE,
-        filter = 'top',
-        options = list(
-          
-        )
-      ) |> 
+      select(FirstAuthor, Year, Journal, Title, Abstract, DOI, Score, Citations)
+    
+    datatable(dt,
+              escape = FALSE,
+              filter = 'top',
+              options = list(
+                columnDefs = list(list(
+                  targets = 0,
+                  render = DT::JS(
+                    "function(data, type, full, meta) {
+                      return '<input type=\"checkbox\" class=\"row-checkbox\"/>';
+                    }"
+                  )
+                ))
+              )
+    ) |> 
       DT::formatStyle(
         columns = "Abstract", 
         fontSize = "11px" 
@@ -66,6 +74,25 @@ server <- function(input, output, session) {
       openxlsx::write.xlsx(bibliography, file, row.names = FALSE)
     }
   )
+  observeEvent(input$bibliography_table_rows_selected, {
+    pmids_selected <- bibliography$pmid[input$bibliography_table_rows_selected]
+    updateTextInput(session, "selected_pmids", value = paste(pmids_selected, collapse = ", "))
+  })
+  
+  observe({
+    shinyjs::runjs(
+      '
+      $(document).on("change", ".row-checkbox", function() {
+        var rowIndex = $(this).closest("tr").index();
+        if(this.checked) {
+          Shiny.setInputValue("bibliography_table_rows_selected", Shiny.inputValues.bibliography_table_rows_selected.concat([rowIndex]), {priority: "event"});
+        } else {
+          Shiny.setInputValue("bibliography_table_rows_selected", Shiny.inputValues.bibliography_table_rows_selected.filter(index => index !== rowIndex), {priority: "event"});
+        }
+      });
+    '
+    )
+  })
   
   observeEvent(input$mots_specifiques_reactive, {
     
